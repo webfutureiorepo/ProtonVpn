7401bb70129f728fbc4e0443c6923a55c171b48f
Shahin Katebi <shahin.katebi@proton.ch>
2025-01-15 15:52:16 +0000

    fix(UI): add UnauthenticatedView
    Jira-Id: VPNAPPL-2591

---
+++ libraries/ConnectWidget/Sources/Widget/Views/UnauthenticatedView.swift
@@ -0,0 +1,47 @@
+//
+//  Created on 15/01/2025.
+//
+//  Copyright (c) 2025 Proton AG
+//
+//  ProtonVPN is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ProtonVPN is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+
+import SwiftUI
+
+struct UnauthenticatedView: View {
+
+    @Environment(\.widgetFamily) var widgetFamily
+
+    var body: some View {
+        VStack {
+            Spacer()
+            switch widgetFamily {
+            case .systemSmall:
+                Image(.logoMarks)
+            default:
+                Image(.logoWithTitle)
+            }
+            Spacer()
+            Button {
+
+            } label: {
+                Text("Sign in")
+            }
+            .buttonStyle(PrimaryButtonStyle())
+        }
+    }
+}
+
+#Preview {
+    UnauthenticatedView()
+}
