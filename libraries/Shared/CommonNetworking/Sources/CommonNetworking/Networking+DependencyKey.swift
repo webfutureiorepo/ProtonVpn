7cde70a738d7dba4ca362fffd89f6376a1d76b07
Chris Janusiewicz <krzysztof.janusiewicz@proton.ch>
2025-02-06 13:05:17 +0000

    fix(ios): build error when building for release
    Jira-Id: VPNAPPL-2621

--- libraries/Shared/CommonNetworking/Sources/CommonNetworking/Networking+DependencyKey.swift
+++ libraries/Shared/CommonNetworking/Sources/CommonNetworking/Networking+DependencyKey.swift
@@ -98,7 +98,9 @@ public struct CoreNetworkingWrapper: VPNNetworking {
 
 /// When using this dependency, make sure `liveValue` owns the only `CoreNetworking` instance.
 public enum VPNNetworkingKey: TestDependencyKey {
-    public static let testValue: VPNNetworking = VPNNetworkingMock()
+    public static let testValue: VPNNetworking = {
+        fatalError("\(Self.self) must have a implementation")
+    }()
 }
 
 #if os(tvOS)
