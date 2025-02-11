cc887ab0501b18acf27cc324159c5e7b4b3a3b1d
Shahin Katebi <shahin.katebi@proton.ch>
2025-02-11 15:02:50 +0000

    fix(intent): fix intents
    Jira-Id: VPNAPPL-2591

--- libraries/NEHelper/Sources/VPNAppCore/Dependency/ConnectionIntentProvider.swift
+++ libraries/NEHelper/Sources/VPNAppCore/Dependency/ConnectionIntentProvider.swift
@@ -17,19 +17,6 @@
 //  along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
 
 import Dependencies
-import AppIntents
-
-/// These intents are never actually used. They are only here in order to be able to invoke them from this package.
-/// The real implementation lives in the iOS app target.
-
-public struct DisconnectFromVPNIntent: AppIntent {
-    public static var title: LocalizedStringResource = "Disconnect from VPN"
-
-    public init() { }
-
-    public func perform() async throws -> some IntentResult {
-        return .result()
-    }
 import DependenciesMacros
 import Domain
 
@@ -41,37 +28,9 @@ public struct ConnectionIntentStorage: TestDependencyKey, Sendable {
     public static let testValue = ConnectionIntentStorage()
 }
 
-public struct ConnectToVPNIntent: AppIntent {
-    public static var title: LocalizedStringResource = "Connect to VPN"
-
-    public init() { }
-
 extension DependencyValues {
     public var connectionIntentStorage: ConnectionIntentStorage {
         get { self[ConnectionIntentStorage.self] }
         set { self[ConnectionIntentStorage.self] = newValue }
-    }
-}
-
-public struct LoginIntent: AppIntent {
-    public static var title: LocalizedStringResource = "Login"
-    public static let openAppWhenRun = true
-
-    public init() { }
-
-    public func perform() async throws -> some IntentResult {
-        return .result()
-    }
-}
-
-public struct LoginIntent: AppIntent {
-    public static var title: LocalizedStringResource = "Login"
-
-    public static var openAppWhenRun = true
-
-    public init() { }
-
-    public func perform() async throws -> some IntentResult {
-        return .result()
     }
 }
