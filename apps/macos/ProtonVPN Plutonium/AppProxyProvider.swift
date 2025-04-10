9cf5085d3f1d5c225c29406f6ef1c8535ba5e440
Shahin Katebi <shahin.katebi@proton.ch>
2025-04-10 12:51:16 +0200

    fix(plutonium): create target
    Jira-Id: VPNAPPL-2565

---
+++ apps/macos/ProtonVPN Plutonium/AppProxyProvider.swift
@@ -0,0 +1,52 @@
+//
+//  Created on 10/04/2025 by Shahin Katebi.
+//
+//  Copyright (c) 2025 Proton AG
+//
+//  Proton VPN is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Proton VPN is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Proton VPN.  If not, see <https://www.gnu.org/licenses/>.
+
+import NetworkExtension
+
+class AppProxyProvider: NEAppProxyProvider {
+
+    override func startProxy(options: [String : Any]?, completionHandler: @escaping (Error?) -> Void) {
+        // Add code here to start the process of connecting the tunnel.
+    }
+    
+    override func stopProxy(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+        // Add code here to start the process of stopping the tunnel.
+        completionHandler()
+    }
+    
+    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)?) {
+        // Add code here to handle the message.
+        if let handler = completionHandler {
+            handler(messageData)
+        }
+    }
+    
+    override func sleep(completionHandler: @escaping () -> Void) {
+        // Add code here to get ready to sleep.
+        completionHandler()
+    }
+    
+    override func wake() {
+        // Add code here to wake up.
+    }
+    
+    override func handleNewFlow(_ flow: NEAppProxyFlow) -> Bool {
+        // Add code here to handle the incoming flow.
+        return false
+    }
+}
