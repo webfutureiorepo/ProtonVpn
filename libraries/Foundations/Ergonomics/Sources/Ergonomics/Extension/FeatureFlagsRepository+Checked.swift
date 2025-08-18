77c701cb16f1c7f32229fc9382094e6aacc1d899
Max Kupetskyi <max.kupetskyi@proton.ch>
2025-08-18 13:32:48 +0200

    fix(credentialless): fix build
    Jira-Id: VPNAPPL-2968

--- libraries/Foundations/Ergonomics/Sources/Ergonomics/Extension/FeatureFlagsRepository+Checked.swift
+++ libraries/Foundations/Ergonomics/Sources/Ergonomics/Extension/FeatureFlagsRepository+Checked.swift
@@ -46,7 +46,7 @@ public class CheckedFeatureFlagsRepository {
     /// Safe wrapper around FeatureFlagsRepository.shared.isEnabled() that asserts if called before fetchFlags()
     public func isEnabled(_ flag: any FeatureFlagTypeProtocol, reloadValue: Bool = false) -> Bool {
         assert(hasFetchedFlags, "Should not call isEnabled() before loading flags")
-        repository.isEnabled(flag, reloadValue: reloadValue)
+        return repository.isEnabled(flag, reloadValue: reloadValue)
     }
 }
 
