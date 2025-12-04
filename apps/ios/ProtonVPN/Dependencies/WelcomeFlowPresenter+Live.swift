f76da5c82b04b26af4c2a6aaa10c4f4ad759997e
Max Kupetskyi <max.kupetskyi@proton.ch>
2025-12-04 16:00:44 +0100

    fix: fix release build

--- apps/ios/ProtonVPN/Dependencies/WelcomeFlowPresenter+Live.swift
+++ apps/ios/ProtonVPN/Dependencies/WelcomeFlowPresenter+Live.swift
@@ -49,8 +49,9 @@ extension WelcomeFlowPresenter: @retroactive DependencyKey {
         overlayViewController: UIViewController?,
         showWelcome: @escaping (String?, UIViewController?) -> Void
     ) {
-        // Check for debug stubs first
-        handleDebugStubs()
+        #if DEBUG
+            handleDebugStubs()
+        #endif
 
         @Dependency(\.windowService) var windowService
 
@@ -62,11 +63,12 @@ extension WelcomeFlowPresenter: @retroactive DependencyKey {
         let environmentsViewController = UIHostingController(rootView: appDebugConfigurationView)
         windowService.show(viewController: environmentsViewController)
     }
+#endif
 
+#if DEBUG
     private func handleDebugStubs() {
         if ProcessInfo.processInfo.environment["ExtAccountNotSupportedStub"] != nil {
             LoginExternalAccountNotSupportedSetup.start()
         }
     }
-
 #endif
