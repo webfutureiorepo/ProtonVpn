fad189ac3226ae67cab3f475cdd759805f4568d6
Max Kupetskyi <max.kupetskyi@proton.ch>
2026-01-14 14:48:52 +0100

    fix: country feature prototyped, city for search
    Jira-Id: VPNAPPL-3286

---
+++ libraries/Features/ios_app/Sources/ios_app/Scenes/Countries/TCA/CityFeature.swift
@@ -0,0 +1,67 @@
+//
+//  Created on 08/01/2026 by Max Kupetskyi.
+//
+//  Copyright (c) 2026 Proton AG
+//
+//  Proton VPN is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Proton VPN is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Proton VPN.  If not, see <https://www.gnu.org/licenses/>.
+
+import ComposableArchitecture
+import Foundation
+import VPNAppCore
+import VPNShared
+
+enum CityFeature {
+    @ObservableState
+    struct State: Equatable, Identifiable {
+        let cityName: String
+        let countryCode: String
+        var servers: IdentifiedArrayOf<ServerItemFeature.State>
+
+        var id: String { "\(cityName)-\(countryCode)" }
+
+        @SharedReader(.vpnConnectionStatus) var vpnConnectionStatus: VPNConnectionStatus
+        @SharedReader(.userTier) var userTier: Int?
+
+        // Computed properties
+        var translatedCityName: String? {
+            servers.first?.translatedCity
+        }
+
+        var displayName: String {
+            translatedCityName ?? cityName
+        }
+
+        var isAnyServerConnected: Bool {
+            servers.contains { $0.isCurrentlyConnected }
+        }
+
+        var isUsersTierTooLow: Bool {
+            servers.allSatisfy(\.isUsersTierTooLow)
+        }
+
+        var underMaintenance: Bool {
+            servers.allSatisfy(\.underMaintenance)
+        }
+
+        var alphaOfMainElements: Double {
+            if underMaintenance {
+                return 0.25
+            }
+            if isUsersTierTooLow {
+                return 0.5
+            }
+            return 1.0
+        }
+    }
+}
